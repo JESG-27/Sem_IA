0f17de65bbc269797ef19d6278511dfec132d38e
JOSE EMMANUEL SALCIDO GUTIERREZ <jose.salcido4178@alumnos.udg.mx>
2023-10-24 09:26:17 -0600

    Resultados DE

--- DE.py
+++ DE.py
@@ -19,53 +19,53 @@ class DE:
     def __init__(self, individuals, iterations):
         self.individuals = individuals
         self.iterations = iterations
-        self.F = np.random.uniform(0.4,0.9)
-        self.crossover = np.random.uniform(0.1, 1)
+        self.crossover = 0.2
         self.population = np.random.uniform(low=[x_min, y_min], high=[x_max , y_max], size=(self.individuals, 2))
-        self.best_solutions = []
+        self.best_solutions = [],[]
         
         minimun = np.argmin([michalewicz(self.population[i]) for i in range(self.individuals)])
-        self.best_global_solution = [self.population[minimun][0], self.population[minimun][1], michalewicz(self.population[minimun])]
-        
+        self.best_global_solution = [self.population[minimun][0], self.population[minimun][1], michalewicz(self.population[minimun])]  
     
     def run(self):
-        cont = 0
-        while(cont < self.iterations):
+        for iteration in range(self.iterations):
             for ind in range(self.individuals):
+                # Mutación
                 r1, r2, r3 = random.sample(range(self.individuals), 3)
-                trial = self.population[r1] + self.F * (self.population[r2] - self.population[r3])
+                F = np.random.uniform(0.4,0.9)
+                trial = self.population[r1] + F * (self.population[r2] - self.population[r3])
                 
+                # Cruce
                 for i in range(2):
-                    if random.random() < self.crossover or i == r1:
+                    if random.random() < self.crossover or i == np.random.randint(0,2):
                         trial[i] = self.population[ind][i]
-            
-                self.borders(trial)
-                if michalewicz(trial) < michalewicz(self.population[ind]):
+
+                # Evaluación
+                if (michalewicz(trial) < michalewicz(self.population[ind]) and self.borders(trial)):
                     self.population[ind] = trial
             
+            # Mejor solución
             minimun = np.argmin([michalewicz(self.population[i]) for i in range(self.individuals)])
-            best_solution = [self.population[minimun][0], self.population[minimun][1], michalewicz(self.population[minimun])]
-            self.best_solutions.append(best_solution)
+            self.best_global_solution = [self.population[minimun][0], self.population[minimun][1], michalewicz(self.population[minimun])]
             
-            if (best_solution[2] < self.best_global_solution[2]):
-                self.best_global_solution = best_solution
-
-            cont += 1
+            self.best_solutions[0].append(self.best_global_solution[2])
+            self.best_solutions[1].append(iteration)
     
     def borders(self, ind):
-        if (ind[0] > x_max):
-            ind[0] = x_max
-        elif (ind[0] < x_min):
-            ind[0] = x_min
+        if (x_min <= ind[0] and ind[0] <= x_max and y_min <= ind[1] and ind[1] <= y_max):
+            return True
+        else:
+            return False
+    
+    def results(self):
+        print("<------------- Resultados ------------->")
+        print(f"Mejores valores: x: {self.best_global_solution[0]} y: {self.best_global_solution[1]}")
+        print(f"Mejor resultado: {self.best_global_solution[2]}")
+        print("<-------------------------------------->")
 
-        if (ind[1] > y_max):
-            ind[0] = y_max
-        elif (ind[1] < y_min):
-            ind[1] = y_min
-
-optimization = DE(50, 100)
+# Evolución Diferencial
+optimization = DE(50, 100) # DE(Individuos, Iteraciones)
 optimization.run()
-print("Best: ", optimization.best_global_solution)
+optimization.results()
 
 # Función
 figura = plt.figure(num="Michalewicz")
@@ -75,6 +75,13 @@ valY = np.linspace(y_min, y_max, 500)
 X, Y = np.meshgrid(valX, valY)
 Z = michalewicz([X, Y])
 ejes.plot_surface(X, Y, Z, cmap="viridis")
-ejes.scatter(optimization.best_global_solution[0], optimization.best_global_solution[1], optimization.best_global_solution[2], marker='*', color='red', s=100)
+evolution = optimization.best_global_solution
+ejes.scatter(evolution[0], evolution[1], evolution[2], marker='*', color='red', s=100)
+
+# Mejores resultados
+figura = plt.figure(num="DE") 
+plt.xlabel("Iteración")
+plt.ylabel("Minimo encontrado")
+plt.plot(optimization.best_solutions[1], optimization.best_solutions[0], color='red')
 
 plt.show()
