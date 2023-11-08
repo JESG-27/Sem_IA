57d1b08e835aa9e9a5d245332d9cfd88aa41bc63
JOSE EMMANUEL SALCIDO GUTIERREZ <jose.salcido4178@alumnos.udg.mx>
2023-11-08 10:49:02 -0600

    Selección Clonal
    Evolución del algoritmo

--- CLONALG.py
+++ CLONALG.py
@@ -19,12 +19,14 @@ class immune():
         self.cells = [[np.random.uniform(x_min, x_max), np.random.uniform(y_min, y_max)] for i in range(population)]
         self.mutation = mutation
         self.cloneRate = cloneRate
-        self.solution = []
+        self.evolution = [],[]
+        
+        minimum = np.argmin([michalewicz(self.cells[i]) for i in range(len(self.cells))])
+        self.best_solution = [self.cells[minimum][0], self.cells[minimum][1], michalewicz(self.cells[minimum])]
 
     def run(self):
         for it in range(self.iterations):
             affinity = []
-            clon_affinity = []
 
             # Evaluación de las soluciones
             for cell in self.cells:
@@ -35,26 +37,28 @@ class immune():
             clones = [[self.cells[i][0], self.cells[i][1]] for i in best_solutions]
         
             # Mutación
-            for clon in clones:
+            mutated_clones = clones
+            for clon in mutated_clones:
                 for i in range(len(clon)):
                     if np.random.random() < self.mutation:
-                        clon[i] += np.random.uniform(-0.5, 0.5)
-                clon_affinity.append(michalewicz(clon))
-            
-            # Mejores soluciones mutadas
-            clon_solutions = np.argsort(clon_affinity)[:len(self.cells)]
-            best_clones = [[clones[i][0], clones[i][1]] for i in clon_solutions]
-            for clon in best_clones:
-                clones.append(clon)
-            
-            remain = len(self.cells)-len(clones)
+                        clon[i] += np.random.uniform(-0.2, 0.2)
+
+            # Nueva población
+            remain = len(self.cells)-(len(clones)*2)
             self.cells = [[np.random.uniform(x_min, x_max), np.random.uniform(y_min, y_max)] for i in range(remain)]
-            for clon in clones:
-                self.cells.append(clon)
+            for clon in range(len(clones)):
+                self.cells.append(clones[clon])
+                self.cells.append(mutated_clones[clon])
 
-        minimum = np.argmin([michalewicz([self.cells[i][0], self.cells[i][1]]) for i in range(len(self.cells))])
-        self.best_solution = [self.cells[minimum][0], self.cells[minimum][1], michalewicz(self.cells[minimum])]
-
+            # Mejor solución encontrada
+            minimum = np.argmin([michalewicz(self.cells[i]) for i in range(len(self.cells))])
+            minimum_result = michalewicz(self.cells[minimum])
+            if (minimum_result < self.best_solution[2]):
+                self.best_solution = [self.cells[minimum][0], self.cells[minimum][1], minimum_result]
+                        
+            # Evolución
+            self.evolution[0].append(it)
+            self.evolution[1].append(self.best_solution[2])
 
 clonal = immune(100, 50, 0.3, 0.1) # immune(iterations, population, mutation, cloneRate)
 clonal.run()
@@ -68,5 +72,11 @@ Z = michalewicz([X, Y])
 fig, ax = plt.subplots(subplot_kw={"projection":"3d"})
 ax.plot_surface(X, Y, Z, cmap="viridis")
 ax.scatter(clonal.best_solution[0], clonal.best_solution[1], clonal.best_solution[2], marker='*', color='red', s=100)
+
+# Mejores resultados
+figura = plt.figure(num="DE") 
+plt.xlabel("Iteración")
+plt.ylabel("Minimo encontrado")
+plt.plot(clonal.evolution[0], clonal.evolution[1], color='red')
 
 plt.show()
